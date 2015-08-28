af65b9ea8d79041aef228e805b9cdae9fd07173b
Paul <paulfcdd@gmail.com>
2015-08-28 16:43:26 +0200

    Dodanie admin panelu

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,10 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Digilife\IndexPlBundle\IndexPlBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
